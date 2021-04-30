00dfa7717a5c51f2239bc45270b9c8109cf7f4f7
Joseph-27 <73574083+Joseph-27@users.noreply.github.com>
2021-04-30 14:17:46 -0700

    edited sidebar and admission

--- bed/includes/bed-sidebar.php
+++ bed/includes/bed-sidebar.php
@@ -545,6 +545,11 @@
             echo ' <a href="#" class="btn btn-link ml-2"><i class="fas fa-calendar-alt text-gray"></i></a>
         <a href="../bed-student/edit.student.php"
         class="btn btn-link pos-right mr-2 text-gray"><i class="fas fa-user-edit"></i></a>';
+        }elseif ($_SESSION['role'] == "Admission") {
+            echo ' <a href="../bed-date/add.date.php" class="btn btn-link ml-1 mr-4"><i class="fas fa-calendar-plus text-gray"></i></a>
+            <a href="../bed-date/set.date.php" class="btn btn-link ml-4 mr-auto"><i class="fas fa-calendar-alt text-gray"></i></a>
+        <a href="../bed-master_key/edit.admission.php?admission_id= ' . $admission_id . '"
+        class="btn btn-link pos-right mr-1 text-gray"><i class="fas fa-user-edit"></i></a>';
         }
         ?>
 
